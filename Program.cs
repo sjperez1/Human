59ee56ee825275665410d7e44a8aedb99d8fde3b
sjperez1 <sarahpz479@gmail.com>
2022-08-02 19:40:36 -0600

    initialize repo

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Human someHuman = new Human("someHuman");
+Human anotherHuman = new Human("anotherHuman", 10, 10, 20, 200);
+anotherHuman.Attack(someHuman);
+
+// namespace creates a little section of files that allows us 
+// allows us to group files together and then allows us to use them in the Program.cs
+//name space is nt necessary in small projects unless you want to make diff groups of files to use
